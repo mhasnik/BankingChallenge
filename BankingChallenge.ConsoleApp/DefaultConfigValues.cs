e51746f0dfbb152337fccbf21a3c1dcd761e4f7e
Michal Hasnik <pasniken@gmail.com>
2021-03-04 15:29:08 +0100

    Added desicion explanation comment

---
+++ BankingChallenge.ConsoleApp/DefaultConfigValues.cs
@@ -0,0 +1,14 @@
+﻿using BankingChallenge.Logic.Models;
+
+namespace BankingChallenge.ConsoleApp
+{
+    // Normally would store this in appSettings.json, but I didn't want
+    // to loose time enforcing fallback, when someone deletes that file
+    class DefaultConfigValues
+    {
+        internal const decimal AdministrativeFeeMaxCap = 10_000;
+        internal const decimal AdministrativeFeeRate = 1;
+        internal const decimal AnnualInterestRate = 5;
+        internal const PaymentInterval PaymentInterval = Logic.Models.PaymentInterval.Monthly;
+    }
+}
